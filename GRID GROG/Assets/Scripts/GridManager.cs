1f043a622d7ec878ac7ceae5d824913b5557e6f4
George Machado <gm2564@nyu.edu>
2019-03-28 12:30:23 -0400

    Created grid with randomized colors

--- GRID GROG/Assets/Scripts/GridManager.cs
+++ GRID GROG/Assets/Scripts/GridManager.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-   const int Rows = 5;
-   const int Cols = 7;
+    public static GridManager Instance;
+   
+   const int Rows = 7;
+   const int Cols = 5;
     
-    
-    
-   private int [,] _gems = new int[Rows,Cols];
+   private int [,] _gems = new int [Cols,Rows];
    public GameObject gemsPrefab;
+   private  Color[] gemColors = new Color[6];
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
+        SetColor();
+        InstantiateGrid();
+    }
+
+    private void SetColor()
+    {
+        gemColors[0] = Color.yellow;
+        gemColors[1] = Color.red;
+        gemColors[2] = Color.blue;
+        gemColors[3] = Color.green;
+        gemColors[4] = Color.magenta;
+        gemColors[5] = Color.cyan;
     }
 
     // Update is called once per frame
@@ -25,14 +38,20 @@ public class GridManager : MonoBehaviour
         
     }
 
-    public void GridSpawn()
+    public void InstantiateGrid()
     {
         for (int x = 0; x < Cols; x++)
         {
             for (int y = 0; y < Rows; y++)
             {
-                int color = Random.Range(0, 9);
-                _gems[x, y] = color;
+
+                int color = Random.Range(0, 6);
+                _gems[x,y] = color;
+                
+                GameObject gem = Instantiate(gemsPrefab);
+                gem.transform.position = new Vector3(x , y );
+                gem.GetComponent<SpriteRenderer>().color = gemColors[color];
+                
             }
         }
     }
