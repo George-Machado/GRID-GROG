72ee0d05e79850054365ced0e145b714e3b4a7a7
George Machado <gm2564@nyu.edu>
2019-03-28 10:30:39 -0400

    IT BEGINS

---
+++ GRID GROG/Assets/Scripts/GridManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+   const int Rows = 5;
+   const int Cols = 7;
+    
+    
+    
+   private int [,] _gems = new int[Rows,Cols];
+   public GameObject gemsPrefab;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GridSpawn()
+    {
+        for (int x = 0; x < Cols; x++)
+        {
+            for (int y = 0; y < Rows; y++)
+            {
+                int color = Random.Range(0, 9);
+                _gems[x, y] = color;
+            }
+        }
+    }
+    
+}
