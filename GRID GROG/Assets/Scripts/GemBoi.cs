44251092d8f3415a3efe6e9fd663ec79dfa5ecf4
George Machado <gm2564@nyu.edu>
2019-04-04 11:55:32 -0400

    Refactored color setting into separate tile script

---
+++ GRID GROG/Assets/Scripts/GemBoi.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBoi : MonoBehaviour
+{
+    const int TileColor = 6;
+
+    private SpriteRenderer _sr;
+
+    private int _type;
+
+    private  Color[] gemColors = new Color[TileColor];
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void SetColor(int t)
+    {
+        gemColors[0] = Color.yellow;
+        gemColors[1] = Color.red;
+        gemColors[2] = Color.blue;
+        gemColors[3] = Color.green;
+        gemColors[4] = Color.magenta;
+        gemColors[5] = Color.cyan;
+
+        _type = t;
+       
+
+        if (_sr == null) {
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        _sr.color = gemColors[_type];
+    }
+
+    public void SetRandomColor()
+    {
+        SetColor(Random.Range(0,gemColors.Length));
+    }
+    
+
+
+
+}
+
+   
+    
+    
+
--- GRID GROG/Assets/Scripts/GridManager.cs
+++ GRID GROG/Assets/Scripts/GridManager.cs
@@ -4,33 +4,26 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public static GridManager Instance;
+   public static GridManager Instance;
    
    const int Rows = 7;
    const int Cols = 5;
     
    private int [,] _gems = new int [Cols,Rows];
+   
    public GameObject gemsPrefab;
-   private  Color[] gemColors = new Color[6];
+   
+   
 
     
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
-        SetColor();
         InstantiateGrid();
     }
 
-    private void SetColor()
-    {
-        gemColors[0] = Color.yellow;
-        gemColors[1] = Color.red;
-        gemColors[2] = Color.blue;
-        gemColors[3] = Color.green;
-        gemColors[4] = Color.magenta;
-        gemColors[5] = Color.cyan;
-    }
+    
 
     // Update is called once per frame
     void Update()
@@ -44,16 +37,14 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < Rows; y++)
             {
-
-                int color = Random.Range(0, 6);
-                _gems[x,y] = color;
-                
                 GameObject gem = Instantiate(gemsPrefab);
-                gem.transform.position = new Vector3(x , y );
-                gem.GetComponent<SpriteRenderer>().color = gemColors[color];
-                
+                gem.GetComponent<GemBoi>().SetRandomColor();
+                gem.transform.position = new Vector2(x , y );
+               
             }
         }
     }
+
+    
     
 }
